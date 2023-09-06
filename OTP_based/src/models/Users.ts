1c2af6fa1528eaec0bb231e11b98f9ef92f2c9f3
Akash Singh <developer.akash8@gmail.com>
2023-09-06 22:27:35 +0600

    model: created for Users

---
+++ OTP_based/src/models/Users.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  OTP: {
+    type: String,
+    required: false,
+  },
+  incorrectAttempt: {
+    type: Number,
+    default: 0,
+  },
+  banned: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
