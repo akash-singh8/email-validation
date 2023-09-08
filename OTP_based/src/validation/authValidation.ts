78418d530f8246bc6e6c722491d30b2135c732c6
Akash Singh <developer.akash8@gmail.com>
2023-09-08 15:26:25 +0600

    validation: added for login data

--- OTP_based/src/validation/authValidation.ts
+++ OTP_based/src/validation/authValidation.ts
@@ -15,3 +15,15 @@ export const signupInputSchema = z.object({
     .min(8, "Password must be at least 8 characters long")
     .max(100, "Password can't exceed 100 characters"),
 });
+
+export const loginInputSchema = z.object({
+  email: z
+    .string()
+    .min(5, "Email must be at least 5 characters long")
+    .max(50, "Email can't exceed 50 characters")
+    .email("Invalid email format"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(100, "Password can't exceed 100 characters"),
+});
