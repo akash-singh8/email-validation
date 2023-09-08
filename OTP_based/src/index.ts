867f6ab32266a2c137682ed5e11e583fcac73702
Akash Singh <developer.akash8@gmail.com>
2023-09-08 11:59:11 +0600

    connect to mongodb

--- OTP_based/src/index.ts
+++ OTP_based/src/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-const app = express();
 dotenv.config();
+const app = express();
 
-const PORT: number = parseInt(process.env.PORT || "3000", 10);
+const mongodb_uri = process.env.MONGODB_URI;
 
-app.listen(PORT, () => {
-  console.log("Server listening on port:", PORT);
-});
+if (!mongodb_uri) {
+  console.error("MONGODB_URI environment variable is not defined.");
+} else {
+  mongoose.connect(mongodb_uri, { dbName: "validator" });
+
+  const db = mongoose.connection;
+
+  db.once("open", () => {
+    console.log("MongoDB connected");
+
+    const PORT: number = parseInt(process.env.PORT || "3000", 10);
+
+    app.listen(PORT, () => {
+      console.log("Server listening on port:", PORT);
+    });
+  });
+
+  db.on("error", (error) => {
+    console.error("MongoDB connection error:", error);
+  });
+}
