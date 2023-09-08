93d65dc23a58127bb417d9403bb0e3f6a04aa9f7
Akash Singh <developer.akash8@gmail.com>
2023-09-08 15:28:32 +0600

    types: added for user login data

--- OTP_based/src/types/authData.ts
+++ OTP_based/src/types/authData.ts
@@ -1,4 +1,9 @@
 import { z } from "zod";
-import { signupInputSchema } from "../validation/authValidation";
+import {
+  signupInputSchema,
+  loginInputSchema,
+} from "../validation/authValidation";
 
 export type SignupUserData = z.infer<typeof signupInputSchema>;
+
+export type LoginUserData = z.infer<typeof loginInputSchema>;
