17004538c7b8cda109cfa76d4126921f397f08ae
Akash Singh <developer.akash8@gmail.com>
2023-09-10 01:10:54 +0600

    controller: added for otp verification

--- OTP_based/src/controllers/otpOperation.ts
+++ OTP_based/src/controllers/otpOperation.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/Users";
 import sendMail from "./sendMail";
+import { otpInputSchema } from "../validation/otpValidation";
 
 export const resendOTP = async (req: Request, res: Response) => {
   const user = req.body.user;
@@ -31,6 +32,55 @@ export const resendOTP = async (req: Request, res: Response) => {
     res
       .status(500)
       .json({ message: "Internal server error while resending OTP" });
+    console.log(err);
+  }
+};
+
+export const verifyOTP = async (req: Request, res: Response) => {
+  const isValidOTP = otpInputSchema.safeParse(req.body);
+
+  if (!isValidOTP.success) {
+    res.status(400).json({
+      message: isValidOTP.error.issues[0].message,
+      error: isValidOTP.error,
+    });
+    return;
+  }
+
+  const userEnteredOTP = parseInt(isValidOTP.data.OTP);
+  const user = req.body.user;
+
+  if (!user) throw new Error("User data not available");
+
+  try {
+    if (!process.env.JWT_OTP_SECRET) {
+      throw new Error("JWT_OTP_SECRET environment variable is not defined.");
+    }
+
+    let OTP_decode;
+
+    try {
+      OTP_decode = jwt.verify(user.OTP, process.env.JWT_OTP_SECRET);
+      if (typeof OTP_decode === "string") throw new Error("Invalid OTP Type");
+    } catch (err) {
+      res.status(404).json({ message: "OTP expired, resend new OTP!" });
+      return;
+    }
+
+    if (userEnteredOTP === OTP_decode.OTP) {
+      await User.updateOne({ _id: user._id }, { verified: true });
+      res.status(201).json({ message: "Successfully verified" });
+    } else {
+      await User.updateOne(
+        { _id: user._id },
+        { $inc: { incorrectAttempt: 1 } }
+      );
+      res.status(404).json({ message: "Invalid OTP" });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Internal server error while verifying OTP" });
     console.log(err);
   }
 };
