24a7e82cbbf555d6e7518c4f06fd6d42fdd42d66
Akash Singh <developer.akash8@gmail.com>
2023-09-09 22:15:11 +0600

    middleware: authenticate jwt for otp requests

---
+++ OTP_based/src/middlewares/authOTP.ts
@@ -0,0 +1,61 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/Users";
+
+export const authenticate_OTP_Req = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token: string = req.headers.authorization
+    ? req.headers.authorization.split(" ")[1]
+    : "";
+
+  if (!token) {
+    res.status(403).json({ message: "Unauthorized" });
+    return;
+  }
+
+  try {
+    if (!process.env.JWT_AUTH_SECRET) {
+      throw new Error("JWT_AUTH_SECRET environment variable is not defined.");
+    }
+
+    let data;
+
+    try {
+      data = jwt.verify(token, process.env.JWT_AUTH_SECRET);
+    } catch (err) {
+      res.status(403).json({ message: "Unauthorized" });
+      return;
+    }
+
+    if (typeof data === "string") throw new Error("Invalid jwt data");
+
+    const user = await User.findById(data.id);
+
+    if (!user || user.banned) {
+      res.status(403).json({
+        message: `The user account is ${user ? "banned" : "deleted"}.`,
+      });
+      return;
+    }
+
+    if (user.OTP_Attempt >= 10) {
+      await User.updateOne({ _id: user._id }, { banned: true });
+
+      res.status(403).json({
+        message: "Too many OTP attempts, the account is Banned!",
+      });
+      return;
+    }
+
+    req.body.user = user;
+    next();
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Internal server error during jwt authentication" });
+    console.log(err);
+  }
+};
