1f7d394fcfaf19eff1d0fb4c58654fb940640791
Akash Singh <developer.akash8@gmail.com>
2023-09-10 11:27:58 +0600

    controller: added to generate link

---
+++ Link_based/backend/src/controllers/linkHandler.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+export const generateLink = (email: string): string | undefined => {
+  try {
+    if (!process.env.JWT_OTP_SECRET) {
+      throw new Error("JWT_OTP_SECRET environment variable is not defined.");
+    }
+
+    if (!process.env.BASE_URL) {
+      throw new Error("BASE_URL environment variable is not defined.");
+    }
+
+    const link = jwt.sign(email, process.env.JWT_OTP_SECRET, {
+      expiresIn: "5m",
+    });
+
+    return process.env.BASE_URL + link;
+  } catch (err) {
+    console.log("Error while generating link");
+    console.error(err);
+  }
+};
