08554417dcd0bd327b4e0bcbb8c8920b5c903be7
Akash Singh <developer.akash8@gmail.com>
2023-09-22 15:20:38 +0600

    authController: add response decorators

--- Docs/src/controllers/authController.ts
+++ Docs/src/controllers/authController.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, Route, SuccessResponse } from "tsoa";
+import {
+  Body,
+  Controller,
+  Example,
+  Post,
+  Route,
+  SuccessResponse,
+  Response,
+} from "tsoa";
 import {
   UsersService,
   UserCreationParams,
@@ -7,7 +15,13 @@ import {
 
 @Route("auth")
 export class AuthController extends Controller {
+  /**
+   * Create a new account for the user..
+   */
   @SuccessResponse("201", "Created")
+  @Response("404", "Invalid Input")
+  @Response("409", "Conflict")
+  @Response("500", "Internal server error")
   @Post("signup")
   public async createUser(@Body() requestBody: UserCreationParams) {
     const newUser = new UsersService();
@@ -17,7 +31,13 @@ export class AuthController extends Controller {
     return result;
   }
 
+  /**
+   * Logs in with the details of an existing user.
+   */
   @SuccessResponse("200", "Success")
+  @Response("403", "Forbidden")
+  @Response("404", "Not Found or Invalid Input")
+  @Response("500", "Internal Server Error")
   @Post("login")
   public async checkUser(@Body() requestBody: UserCheckParams) {
     const newUser = new UsersService();
