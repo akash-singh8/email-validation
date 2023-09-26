a24fd97048f5fe1a96b4607681f5f17dda5af41f
Akash Singh <developer.akash8@gmail.com>
2023-09-26 10:54:11 +0600

    validation check before resending OTP

--- Docs/src/services/otpService.ts
+++ Docs/src/services/otpService.ts
@@ -1,17 +1,20 @@
 import jwt from "jsonwebtoken";
 import User from "../models/Users";
 import sendMail from "../controllers/sendMail";
+import { validationCheck } from "./linksService";
+import { UserPayloadData } from "./linksService";
 
 export class OTPService {
-  public resend = async (user: {
-    id: string;
-    email: string;
-    totalAttempts: number;
-  }) => {
+  public resend = async (user: UserPayloadData) => {
     try {
       if (!user) throw new Error("User data not available");
       if (!process.env.JWT_OTP_SECRET)
         throw new Error("JWT_OTP_SECRET environment variable is not defined.");
+
+      const userCheck = await validationCheck(user);
+      if (userCheck.status !== 200) {
+        return userCheck;
+      }
 
       const new_OTP = Math.floor(Math.random() * 999999);
       const encrypted_OTP = jwt.sign(
@@ -22,13 +25,32 @@ export class OTPService {
         }
       );
 
-      await sendMail(user.email, new_OTP);
+      await sendMail(user, new_OTP);
       await User.updateOne(
         { _id: user.id },
-        { OTP: encrypted_OTP, totalAttempts: user.totalAttempts + 1 }
+        { OTP: encrypted_OTP, totalAttempts: user.attempts + 1 }
       );
 
-      return { status: 201, message: `OTP resend to ${user.email}` };
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          verified: false,
+          banned: false,
+          totalAttempts: user.attempts + 1,
+        },
+        process.env.JWT_AUTH_SECRET!,
+        {
+          expiresIn: "1h",
+        }
+      );
+
+      return {
+        status: 201,
+        message: `OTP resend to ${user.email}`,
+        authToken: token,
+      };
+      // send new token
     } catch (err) {
       console.log(err);
       return {
